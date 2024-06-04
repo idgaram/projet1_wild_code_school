1a07eb2b5a04a245379b87fa8a3087dfc4b953e4
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-04 15:46:58 +0200

    création du bloc connection

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ bouton.addEventListener("click", () => {
 fermerFlorian.addEventListener("click", () => {
   messageFlorian.classList.toggle("visible");
 });
+
+const connectionPopup = document.querySelector(".connection");
+const boutonLogin = document.querySelector("#identify");
+const connectionFermer = document.querySelector("#connection-fermer");
