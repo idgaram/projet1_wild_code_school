6f9acfa9e9d71386ffbb4e65308b48d4195c0280
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-04 21:00:15 +0200

    ajout du lien

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ const textDevDeux =
 console.log(intro);
 console.log(textDevUn);
 console.log(textDevDeux);
+
+// fonction login à répéter
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#login-index");
+  const inputField = document.querySelector(".password-field");
+  const answer = "abdou123456789";
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (inputField.value === answer) {
+      window.location.href = "sapeur.html";
+    } else {
+      alert("le mot de passe est erroné");
+    }
+  });
+});
--- script_sapeur.js
+++ script_sapeur.js
@@ -20,3 +20,19 @@ boutonPause.addEventListener("click", () => {
 // function pauseAudio() {
 //   x.pause();
 // }
+// fonction login à répéter
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#login-index");
+  const inputField = document.querySelector(".password-field");
+  const answer = "abdou123456789";
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (inputField.value === answer) {
+      window.location.href = "sapeur.html";
+    } else {
+      alert("le mot de passe est erroné");
+    }
+  });
+});
