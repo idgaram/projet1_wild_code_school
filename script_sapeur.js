72982a6c94060b48f4360b7b76a11e49e0c776c6
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-05 09:31:26 +0200

    emplacement mesage

--- script_sapeur.js
+++ script_sapeur.js
@@ -12,27 +12,49 @@ boutonPause.addEventListener("click", () => {
   musique.pause();
 });
 
-// function playAudio() {
-//   musique.play();
-//   //   alert("click");
-// }
+const connectionPopup = document.querySelector(".connection");
+const boutonLogin = document.querySelector("#identify");
+const connectionFermer = document.querySelector(".connection-fermer");
 
-// function pauseAudio() {
-//   x.pause();
-// }
+boutonLogin.addEventListener("click", () => {
+  connectionPopup.classList.toggle("visible");
+});
+
+connectionFermer.addEventListener("click", () => {
+  connectionPopup.classList.toggle("visible");
+});
+
+const reponse = document.querySelector(".reponse");
+const monfils = document.querySelector(".monfils");
+const reponseFermer = document.querySelector(".reponse-fermer");
+
+monfils.addEventListener("click", () => {
+  reponse.classList.toggle("visible");
+});
+
+reponseFermer.addEventListener("click", () => {
+  reponse.classList.toggle("visible");
+});
+
+reponse - fermer;
 // fonction login à répéter
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#login-index");
   const inputField = document.querySelector(".password-field");
-  const answer = "abdou123456789";
+  const answer = "Sapé comme jamais";
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     if (inputField.value === answer) {
-      window.location.href = "sapeur.html";
+      window.location.href = "graphiste.html";
     } else {
       alert("le mot de passe est erroné");
     }
   });
 });
+
+console.log(
+  "clique sur le bouton follow de l'enfant de l'homme qui est le père de ton fils, du coup sachant que ton fils est le tien, que l'homme est le grand père de ton fils, et que tu es un homme. J'en déduis que tu devrait etre le fils du grand père de ton fils, je dirait donc que l'homme est ton père et que tu es son fils"
+);
